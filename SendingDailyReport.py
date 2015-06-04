18381377885cdda2d95a02a6c07f90f760d34155
mingchen7 <mingchen7@email.arizona.edu>
2015-06-04 13:57:22 -0700

    modified sendingdailyreport

--- SendingDailyReport.py
+++ SendingDailyReport.py
@@ -16,8 +16,8 @@ from email.MIMEText import MIMEText
 import SqlConnect
 
 
-# Generating sting for the daily report email
-# Version 1
+# Generate report content
+# Version 1 - for daily report
 def GenerateReport(Reportday,StationList):
     strReport = ""
     dicTotal = {}
@@ -34,11 +34,7 @@ def GenerateReport(Reportday,StationList):
         BeginTime = Reportday + datetime.timedelta(hours = Hour)
         EndTime = BeginTime + datetime.timedelta(hours = 1)
         BeginTime = BeginTime.strftime("%Y-%m-%d %H:%M:%S")
-        EndTime = EndTime.strftime("%Y-%m-%d %H:%M:%S")
-        
-        #TimeString = "%s to %s\n" % (BeginTime,EndTime)
-        #strReport = strReport + TimeString
-        #print "%s to %s" % (BeginTime,EndTime)
+        EndTime = EndTime.strftime("%Y-%m-%d %H:%M:%S")            
         
         FilePath = 'Raw Data/{0:04d}/{0:04d}_{1:02d}/{0:04d}_{1:02d}_{2:02d}'.format(Year, Month, Day)
         FileName = 'Raw Data/{0:04d}/{0:04d}_{1:02d}/{0:04d}_{1:02d}_{2:02d}/{0:04d}_{1:02d}_{2:02d}_{3:02d}.txt'.format(Year, Month, Day, Hour)
@@ -65,19 +61,58 @@ def GenerateReport(Reportday,StationList):
             dicTotal[location] = dicTotal[location] + dic[location] 
          
     strReport = strReport + "\nTotal Sample Counts:\n"
-    for location in dicTotal:
-        #   print "%s, %d" % (location, dic[location]) 
+    for location in dicTotal:        
             line = "  %s, %d\n" % (location, dicTotal[location])
             strReport = strReport + line
                 
     strReport = strReport + "\n"    
     return strReport
 
+
+# Getting total sample size for each location each day
+# Version 2 - for history report
+def GetTotalSamples(Reportday,StationList):    
+    dicTotal = {}
+    
+    for location in StationList:
+        dicTotal[location] = 0;
+    
+    Year = Reportday.year
+    Month = Reportday.month
+    Day = Reportday.day
+    
+    for Hour in range(24):
+
+        BeginTime = Reportday + datetime.timedelta(hours = Hour)
+        EndTime = BeginTime + datetime.timedelta(hours = 1)
+        BeginTime = BeginTime.strftime("%Y-%m-%d %H:%M:%S")
+        EndTime = EndTime.strftime("%Y-%m-%d %H:%M:%S")
+               
+        FilePath = 'Raw Data/{0:04d}/{0:04d}_{1:02d}/{0:04d}_{1:02d}_{2:02d}'.format(Year, Month, Day)
+        FileName = 'Raw Data/{0:04d}/{0:04d}_{1:02d}/{0:04d}_{1:02d}_{2:02d}/{0:04d}_{1:02d}_{2:02d}_{3:02d}.txt'.format(Year, Month, Day, Hour)
+        if(os.path.exists(path = FilePath)):
+            if(os.path.isfile(FileName)):
+                f = open(FileName,'r')
+            else:
+                print "File %s does not exist!\n" % (FileName)                
+                continue
+        else:
+            print "File path %s does not exist!\n" % (FilePath)
+            return -1
+                        
+        SS = CheckStationStatus.StationStatus(StationList)
+        dic = SS.checkStatus(f)     
+                                        
+        for location in dic: #adding the total day sample counts
+            dicTotal[location] = dicTotal[location] + dic[location] 
+          
+    return dicTotal
+
+
 def SendingEmail(email,password,message,date):
     SMTPserver = 'smtp.gmail.com'
     sender = email
-    receivers = ['mingchen7@email.arizona.edu','shuyang@email.arizona.edu']
-    #receivers = ['matthewchen719@gmail.com','mingchen7@email.arizona.edu']
+    receivers = ['mingchen7@email.arizona.edu','shuyang@email.arizona.edu']    
     
     # typical values for text_subtype are plain, html, xml
     text_subtype = 'plain'
@@ -105,70 +140,26 @@ def SendingEmail(email,password,message,date):
     except Exception, exc:
         sys.exit( "mail failed; %s" % str(exc) ) # give a error message
 
-def Report():
-    Speedway = ('Speedway-Euclid','Speedway-Park','Speedway-Mountain','Speedway-Cherry','Speedway-Campbell','Speedway-Tucson','Speedway-Country','Speedway-ElRancho','Speedway-Alvernon','Speedway-Columbus','Speedway-Swan','Speedway-Rosemont','Speedway-Carycroft','Speedway-Wilmot','Speedway-Kolb','Speedway-Prudence','Speedway-Pantano','Speedway-Camino','Speedway-Harrison')
-    Broadway = ('Broadway-Aviation','Broadway-Euclid','Broadway-Highland','Broadway-Campbell','Broadway-Tucson','Broadway-CountryClub','Broadway-Randolph','Broadway-Dodge','Broadway-Alvernon')
-    Oracle = ('Grant-Oracle','Prince-Oracle','Wetmore-Oracle')
+
+def DailyReport(Corridors):    
     Today = datetime.datetime.today()
     Yesterday = Today + datetime.timedelta(days = -1)
     Year = Yesterday.year
     Month = Yesterday.month
     Day = Yesterday.day
     
-    Reportday = datetime.datetime(Year,Month,Day)
-    #Reportday = datetime.datetime(2014,9,4)
-    msg = "Speedway:\n"
-    msg = msg + GenerateReport(Reportday, Speedway)
-    msg = msg + "Broadway:\n"
-    msg = msg + GenerateReport(Reportday, Broadway)
-    msg = msg + "Oracle:\n"
-    msg = msg + GenerateReport(Reportday, Oracle)
+    # Reportday = datetime.datetime(Year,Month,Day)
+    Reportday = datetime.datetime(2015,3,24)
+    
+    msg = ""
+    
+    for Corridor in Corridors.keys():
+        msg = msg + "%s:\n" % (Corridor);
+        msg = msg + GenerateReport(Reportday, Corridors[Corridor])
+        
     SendingEmail('smarttranslab@gmail.com', 'googleaccountpwd', msg, Reportday)
-
-# For just calculating the total sample size received each day of at the BT device locations
-# Version 2
-def GetTotalSamples(Reportday,StationList):    
-    dicTotal = {}
-    
-    for location in StationList:
-        dicTotal[location] = 0;
-    
-    Year = Reportday.year
-    Month = Reportday.month
-    Day = Reportday.day
-    
-    for Hour in range(24):
-
-        BeginTime = Reportday + datetime.timedelta(hours = Hour)
-        EndTime = BeginTime + datetime.timedelta(hours = 1)
-        BeginTime = BeginTime.strftime("%Y-%m-%d %H:%M:%S")
-        EndTime = EndTime.strftime("%Y-%m-%d %H:%M:%S")
-               
-        FilePath = 'Plant Files/{0:04d}/{0:04d}_{1:02d}/{0:04d}_{1:02d}_{2:02d}'.format(Year, Month, Day)
-        FileName = 'Plant Files/{0:04d}/{0:04d}_{1:02d}/{0:04d}_{1:02d}_{2:02d}/{0:04d}_{1:02d}_{2:02d}_{3:02d}.txt'.format(Year, Month, Day, Hour)
-        if(os.path.exists(path = FilePath)):
-            if(os.path.isfile(FileName)):
-                f = open(FileName,'r')
-            else:
-                print "File %s does not exist!\n" % (FileName)                
-                continue
-        else:
-            print "File path %s does not exist!\n" % (FilePath)
-            return -1
-                        
-        SS = CheckStationStatus.StationStatus(StationList)
-        dic = SS.checkStatus(f)     
-                                        
-        for location in dic: #adding the total day sample counts
-            dicTotal[location] = dicTotal[location] + dic[location] 
-          
-    return dicTotal
-    
-def HistoryReport():
-    Speedway = ('Speedway-Euclid','Speedway-Park','Speedway-Mountain','Speedway-Cherry','Speedway-Campbell','Speedway-Tucson','Speedway-Country','Speedway-ElRancho','Speedway-Alvernon','Speedway-Columbus','Speedway-Swan','Speedway-Rosemont','Speedway-Carycroft','Speedway-Wilmot','Speedway-Kolb','Speedway-Prudence','Speedway-Pantano','Speedway-Camino','Speedway-Harrison')
-    Broadway = ('Broadway-Aviation','Broadway-Euclid','Broadway-Highland','Broadway-Campbell','Broadway-Tucson','Broadway-CountryClub','Broadway-Randolph','Broadway-Dodge','Broadway-Alvernon')
-    Oracle = ('Grant-Oracle','Prince-Oracle','Glenn-Oracle','MiracleMile-Oracle','Prince-Oracle','Roger-Oracle','Limberlost-Oracle','Wetmore-Oracle','TucsonMall-Oracle','River-Oracle')
-    
+        
+def HistoryReport(Corridors):
     Start_Year = 2014
     Start_Month = 9
     Start_Day = 1
@@ -184,35 +175,46 @@ def HistoryReport():
     dicIntID = Query.GetIntersectionTable()
     
     while TempDate <= EndDate:
-        dicTotal = GetTotalSamples(TempDate, Oracle)
-        if dicTotal != -1:        
-            for location in dicTotal: 
-                INT_id = dicIntID[location]
-                ReportDate = TempDate
-                
-                if dicTotal[location] > 0:
-                    BTStatus = 1
-                else:
-                    BTStatus = 0
-                
-                TotalSamples = dicTotal[location]
-    
-                Query.InsertBTStatusRecord(INT_id, ReportDate, BTStatus, TotalSamples)
-                print "%s, %s" % (TempDate,location)
+        for corridor in Corridors:            
+            dicTotal = GetTotalSamples(TempDate, corridor)
+            if dicTotal != -1:        
+                for location in dicTotal: 
+                    INT_id = dicIntID[location]
+                    ReportDate = TempDate
+                    
+                    if dicTotal[location] > 0:
+                        BTStatus = 1
+                    else:
+                        BTStatus = 0
+                    
+                    TotalSamples = dicTotal[location]
         
+                    Query.InsertBTStatusRecord(INT_id, ReportDate, BTStatus, TotalSamples)
+                    print "%s, %s" % (TempDate,location)
+            
         TempDate = TempDate + datetime.timedelta(days = 1)
 
-def main():
-    #Report()
-    HistoryReport()
+def main():    
+    Corridors = {}
+    Speedway = ('Speedway-Euclid','Speedway-Park','Speedway-Mountain','Speedway-Cherry','Speedway-Campbell','Speedway-Tucson','Speedway-Country', \
+                'Speedway-ElRancho','Speedway-Alvernon','Speedway-Columbus','Speedway-Swan','Speedway-Rosemont','Speedway-Carycroft','Speedway-Wilmot',\
+                'Speedway-Kolb','Speedway-Prudence','Speedway-Pantano','Speedway-Camino','Speedway-Harrison')
     
-    """
+    Broadway = ('Broadway-Aviation','Broadway-Euclid','Broadway-Highland','Broadway-Campbell','Broadway-Tucson','Broadway-CountryClub','Broadway-Randolph','Broadway-Dodge','Broadway-Alvernon')    
+    Oracle = ('Grant-Oracle','Glenn-Oracle','Miraclemile-Oracle','Prince-Oracle','Roger-Oracle','Limberlost-Oracle','Wetmore-Oracle','TucsonMall-Oracle','River-Oracle');
+              
+    Corridors["Speedway"] = Speedway;
+    Corridors["Broadway"] = Broadway;
+    Corridors["Oracle"] = Oracle; 
+    
+    # DailyReport(Corridors)
+    # HistoryReport(Corridors)
+    
+    
     while True:
         time.sleep(1 * 3600)        
         if (datetime.datetime.now().hour == 8):            
-            Report()
-    """
-    print "Done"
+            DailyReport(Corridors)        
         
 if __name__ == "__main__":
     main(); 
